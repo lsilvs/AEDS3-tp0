7d6b6fc7a712d58f511ec842e886c8f3d804fcb1
Lucas Silvestre <lukas.silvestre@gmail.com>
2012-09-20 22:02:20 -0300

    Comentários + liberação de memória

--- src/tp0.c
+++ src/tp0.c
@@ -9,12 +9,12 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-
+	// Declara as variáveis
 	char * inputFileName = "input.txt";
 	char * outputFileName = "output.txt";
 	int numMatrizes, numLinhas1, numLinhas2, numLinhasResult, numColunas1, numColunas2, numColunasResult, i, j, k, l, m;
 
-	//Abrir arquivo de entrada
+	// Abrir arquivo de entrada
 	FILE * inputFileOpen = NULL;
 	if ((inputFileOpen = fopen(inputFileName, "r")) == NULL) {
 		printf("Nao foi possivel abrir o arquivo.\n");
@@ -22,7 +22,7 @@ int main(int argc, char *argv[]) {
 		printf("Aberto arquivo de entrada %s \n", inputFileName);
 	}
 
-	//Abrir arquivo de saída
+	// Abrir arquivo de saída
 	FILE * outputFileOpen = NULL;
 	if ((outputFileOpen = fopen(outputFileName, "w")) == NULL) {
 		printf("Nao foi possivel abrir o arquivo.\n");
@@ -30,102 +30,91 @@ int main(int argc, char *argv[]) {
 		printf("Aberto arquivo de entrada %s \n", outputFileName);
 	}
 
+	// Lê o número de instâncias
 	fscanf(inputFileOpen, "%d", &numMatrizes);
+	// Escreve o número de instâncias no arquivo de saída
 	fprintf(outputFileOpen, "%d\n", numMatrizes);
 
 	for(m = 0; m < numMatrizes; m++) {
+		// Lê linhas e colunas da matriz
 		fscanf(inputFileOpen, "%d", &numLinhas1);
 		fscanf(inputFileOpen, "%d", &numColunas1);
 
+		// Aloca a matriz
 		int ** matrizA = (int **) malloc(numLinhas1 * sizeof(int *));
-
 		for(i = 0; i < numLinhas1; i++) {
 			matrizA[i] = (int *) malloc(numColunas1 * sizeof(int));
-		}	
+		}
 
+		// Preenche a matriz
 		for(i = 0; i < numLinhas1; i++) {
 			for(j = 0; j < numColunas1; j++) {
 				fscanf(inputFileOpen, "%d", &matrizA[i][j]);
 			}
 		}
 
+		// Lê linhas e colunas da matriz
 		fscanf(inputFileOpen, "%d", &numLinhas2);
 		fscanf(inputFileOpen, "%d", &numColunas2);
 
+		// Aloca a matriz
 		int ** matrizB = (int **) malloc(numLinhas2 * sizeof(int *));
-
 		for(i = 0; i < numLinhas2; i++) {
 			matrizB[i] = (int *) malloc(numColunas2 * sizeof(int));
-		}	
+		}
 
+		// Preenche a matriz
 		for(i = 0; i < numLinhas2; i++) {
 			for(j = 0; j < numColunas2; j++) {
 				fscanf(inputFileOpen, "%d", &matrizB[i][j]);
 			}
 		}
 
+		// Aloca a matriz
 		int ** matrizResult = (int **) malloc(numLinhas1 * numLinhas2 * sizeof(int *));
-
 		for(i = 0; i < numLinhas1 * numLinhas2; i++) {
 			matrizResult[i] = (int *) malloc(numColunas1 * numColunas2 * sizeof(int));
 		}
 
+		// Preenche a matriz
 		for (i = 0; i < numLinhas1; ++i) {
 			for (j = 0; j < numColunas1; ++j) {
 				for (k = 0; k < numLinhas2; ++k) {
 					for (l = 0; l < numColunas2; ++l) {
 						matrizResult[i * numLinhas2 + k][j * numColunas2 + l] = matrizA[i][j] * matrizB[k][l];
-						// printf("%s\n", matrizA[i][j] * matrizB[k][l]);
 					}
 				}
 			}
 		}
 
+		// Libera as matrizes não utilizadas
+		for(i = 0; i < numLinhas1; i++) {
+			free(matrizA[i]);
+		}
+		free(matrizA);
 
-		// Imprime a matriz
+		for(i = 0; i < numLinhas2; i++) {
+			free(matrizB[i]);
+		}
+		free(matrizB);
+
+		numLinhasResult = numLinhas1 * numLinhas2;
+		numColunasResult = numColunas1 * numColunas2;
+
+		// Escreve o número de linhas e colunas no arquivo de saída
+		fprintf(outputFileOpen, "%d ", numLinhasResult);
+		fprintf(outputFileOpen, "%d\n", numColunasResult);
+
+		// Escreve a matriz no arquivo de saída
 		for(i = 0; i < numLinhas1 * numLinhas2; i++) {
 			for(j = 0; j < numColunas1 *numColunas2; j++) {
-				printf("%d ", matrizResult[i][j]);
+				fprintf(outputFileOpen,"%d ", matrizResult[i][j]);
 			}
-			printf("\n");
+			fprintf(outputFileOpen, "\n");
 		}
-
-		// numLinhasResult = numLinhas1 * numLinhas2;
-		// numColunasResult = numColunas1 * numColunas2;
-
-		// fprintf(outputFileOpen, "%d ", numLinhasResult);
-		// fprintf(outputFileOpen, "%d\n", numColunasResult);
-
-		// for (i = 0; i < numColunas1; i++) {
-		// 	for (j = 0; j < numColunas2; j++) {
-		// 		fprintf(outputFileOpen, "%d ", matrizA[i] * matrizB[j]);
-		// 	}
-		// }
-
-		// fprintf(outputFileOpen, "\n");
-
-		// for (i = 0; i < numColunas1; i++) {
-		// 	for (j; j < numColunasResult; j++) {
-		// 		fprintf(outputFileOpen, "%d ", matrizA[i] * matrizB[j]);
-		// 	}
-		// }
-
-		fprintf(outputFileOpen, "\n");
 	}
-	
-
-
-
-	/* Allocate space for an array with ten elements of type int. */
-	// int * vetor = (int *) malloc(n * sizeof(int));
-	
+		
 	// free(vetor);
-	
-	// 	fprintf(file, "%s", value);
-
-	// FEOF
-	//retorna se é o fim do arquivo ou não
-	//return feof(file);
 
 	//retorna 0 se conseguiu fechar o arquivo com sucesso
 	printf("Fechando arquivo...\n");
